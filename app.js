5da3099679b964566bd4fea14a29613dea2d4fa6
dev-jv <dev.javierleon@gmail.com>
2020-12-01 18:01:03 -0500

    four

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const { crearArchivo } = require('./multiplicar/multiplicar')
+const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let base = '3';
+    // let base = '3';
+
+// console.log(process.argv); // [C.., D...]
+let argv = process.argv;
+let parametro = argv[2]; // --base=x
+let base = parametro.split('=')[1] // x
+
+// console.log(base);
 
 crearArchivo(base)
     .then( archivo => console.log(`Archivo creado: ${ archivo }`))
