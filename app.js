2593e21dc48e776189147ba55bb68c84ab78ea4c
dev-jv <dev.javierleon@gmail.com>
2020-12-01 15:36:54 -0500

    two

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-let base = 2;
+// Requireds
+const fs = require('fs');
+// const express = require('express');
+// const fs = require('./fs');
 
-for(let i = 1; i<10; i++){
-    console.log(` <${i}>`, ` ${base} * ${i} =`, base*i);
+let base = 2;
+let data = '';
+
+for(let i = 1; i<16; i++){
+    data += `${base} * ${i} = ${base*i}\n`;
 }
+
+fs.writeFile(`tablas/T-${base}.txt`, data, (err) => {
+  if (err) throw err;
+  console.log(`The file T-${base} has been created`);
+});
+
+
+
