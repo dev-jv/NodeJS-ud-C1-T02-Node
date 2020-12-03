cf5cd95b442e027291cb867f174b86b8686cb6c2
dev-jv <dev.javierleon@gmail.com>
2020-12-03 11:57:23 -0500

    six

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
+// --------------------------------------------------------------- <> process.argv > 'node app --base=x'
+// const { crearArchivo } = require('./multiplicar/multiplicar');
+
+// // let base = '3';
+
+// // console.log(process.argv); // [C.., D...]
+// let argv = process.argv;
+// let parametro = argv[2]; // --base=x
+// let base = parametro.split('=')[1] // x
+
+// // console.log(base);
+
+// crearArchivo(base)
+// .then( archivo => console.log(`Archivo creado: ${ archivo }`))
+// .catch( err => console.log(err))
+
+// --------------------------------------------------------------------------------- <> yargs > 'node app listar -b x -l y'
+const argv = require('yargs')
+                .command('listar', 'Imprime en consola la tabla de multiplicar', { 
+                    base : {
+                        demand: true,
+                        alias: 'b', 
+                    },
+                    limite: {
+                        alias: 'l',
+                        default: 10,
+                    }
+                })
+                .help()
+                .argv;
+
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-    // let base = '3';
 
-// console.log(process.argv); // [C.., D...]
-let argv = process.argv;
-let parametro = argv[2]; // --base=x
-let base = parametro.split('=')[1] // x
+// let argv2 = process.argv;
 
-// console.log(base);
+// console.log(argv2); // Lo normal ...
+// console.log(argv); // Con yargs
 
-crearArchivo(base)
-    .then( archivo => console.log(`Archivo creado: ${ archivo }`))
-    .catch( err => console.log(err))
+console.log( ' Base', argv.base);
+console.log( ' Limite', argv.limite);
+
+
+// 'node app listar --base x --limite y'
+// 'node app listar -b x -l y'
+// 'node app listar --help
+// 'node app --help
 
 
 
